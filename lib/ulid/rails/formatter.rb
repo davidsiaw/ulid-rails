b559d95e1ab0da65b3f7c440faa9dbe2f0e499d9
David Siaw <davidsiaw@gmail.com>
2019-02-05 02:43:34 +0900

    pgsql compat

--- lib/ulid/rails/formatter.rb
+++ lib/ulid/rails/formatter.rb
@@ -4,11 +4,23 @@ module ULID
   module Rails
     module Formatter
       def self.format(v)
-        v.length == 32 ? Base32::Crockford.encode(v.hex) : v
+        if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter) &&
+          ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
+          v.length == 32 ? Base32::Crockford.encode(v.hex) : v
+        elsif defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) && 
+          ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+          Base32::Crockford.encode(v.delete('-').to_i(16))
+        end
       end
 
       def self.unformat(v)
-        Base32::Crockford.decode(v).to_s(16).rjust(32, "0")
+        if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter) &&
+          ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::MysqlAdapter)
+          Base32::Crockford.decode(v).to_s(16).rjust(2, '0')
+        elsif defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) && 
+          ActiveRecord::Base.connection.instance_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+          [Base32::Crockford.decode(v).to_s(16).rjust(2, '0')].pack('H*')
+        end
       end
     end
   end
